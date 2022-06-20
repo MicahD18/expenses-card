3744cd30632bb527fb05cf7d74b488fbc2bcf290
MicahD18 <micah.daise@nwktc.edu>
2022-06-19 18:22:22 -0600

    added data to ui

--- client/src/components/ExpenseCard.jsx
+++ client/src/components/ExpenseCard.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import { weekExpenses } from "./data/weekData";
+
 import "./ExpenseCard.css";
 
 import image from "../assets/circles.png";
 
 const ExpenseCard = () => {
+  const [expenses, setExpenses] = useState([]);
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("Err: ", err));
+  }, []);
+
   return (
     <div className="card__container">
       <div></div>
@@ -11,16 +24,26 @@ const ExpenseCard = () => {
         <div className="balance__bar">
           <div className="balance__section">
             <p>My balance</p>
-            <h1>$000.00</h1>
+            <h1>$11,962.50</h1>
           </div>
           <img src={image} alt="logo" className="logo" />
         </div>
         <div className="expense__card">
           <h3>Spending - Last 7 days</h3>
+
+          <div className="week__expense">
+            {weekExpenses.map((expense) => (
+              <div className="week__expense-content">
+                <h4>${expense.amount}</h4>
+                <p>{expense.day}</p>
+              </div>
+            ))}
+          </div>
+
           <div className="footer">
             <div className="monthly">
               <p>Total this month</p>
-              <h1>$000.00</h1>
+              <h1>$263.39</h1>
             </div>
             <div className="last__month">
               <h4>+2.4%</h4>
